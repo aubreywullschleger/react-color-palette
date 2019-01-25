f633fd4b382c1064666c934e739ca0e6ff885195
aubreywullschleger <mentor-aubrey@codecademy.com>
2019-01-24 17:12:49 -0700

    simple color picker app, basic functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function ColorOption(props) {
+  return (
+    <button
+      className="color-option"
+      value={props.value}
+      style={{ backgroundColor: props.value }}
+      onClick={props.onClick}
+    >
+    </button>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function ColorSelection(props) {
+  return (
+    <div
+      className="color-selection"
+      style={{ backgroundColor: props.value }}
+    >
+      <h1>{props.value}</h1>
+    </div>
+  )
+}
+
+class Palette extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorOptions: ['red', 'green', 'blue', 'yellow'],
+      colorSelection: '',
+    }
+    // this.colorOptionsList = this.colorOptionsList.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  colorOptionsList() {
+    return this.state.colorOptions.map(color => {
+        return (
+          <ColorOption
+            key={color}
+            value={color}
+            onClick={(e) => this.handleClick(e)}
+          />
+        );
+      }
+    );
+  }
+
+  handleClick(e) {
+    this.setState({ colorSelection: e.target.value }) 
+  }
+
+  render() {
+    return (
+      <main className="palette">
+        <section className="color-choices">
+          <h1>Choose a color:</h1>
+          {this.colorOptionsList()}
+        </section>
+        <section className="color-choice-container">
+          <h1>Color choice:</h1>
+          <ColorSelection value={this.state.colorSelection} />
+        </section>
+      </main>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Palette />,
+  document.getElementById('root')
+);
